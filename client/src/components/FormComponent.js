c04a1aa42e69eb086bd17913efce1dd864d85f92
mintmintra <mintracss019@gmail.com>
2022-08-24 23:18:01 +0700

    sweetalert2

--- client/src/components/FormComponent.js
+++ client/src/components/FormComponent.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import NavbarComponent from "./NavbarComponent";
 import axios from "axios";
+import Swal from 'sweetalert2'
 const FormComponent = () => {
   const [state, setState] = useState({
     title: "",
@@ -19,9 +20,18 @@ const FormComponent = () => {
     console.log("API URL",process.env.REACT_APP_API)
     axios.post(`${process.env.REACT_APP_API}/create`,{title,content,author})
     .then(response=> {
-      alert("บันทึกข้อมูลเรียบร้อย")
+      Swal.fire(
+        'แจ้งเตือน',
+        'บันทึกข้อมูลบทความเรียบร้อย',
+        'success'
+      )
+      setState({...state,title:"",content:"",author:""})
     }).catch(err=>{
-      alert(err.response.data.error)
+      Swal.fire(
+        'แจ้งเตือน',
+        err.response.data.error,
+        'error'
+      )
     })
     }
 
