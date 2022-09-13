04141f9fc7c78d75ea4f66916cf8621bbd3fbaa8
mintmintra <mintracss019@gmail.com>
2022-09-13 20:11:51 +0700

    component client

--- client/src/components/SingleComponent.js
+++ client/src/components/SingleComponent.js
@@ -1,6 +1,25 @@
-const SingleComponent = () => {
+import axios from 'axios';
+import { useState, useEffect } from "react";
+import NavbarComponent from "./NavbarComponent";
+const SingleComponent = (props) => {
+    const [blog,setBlog] = useState('')
+
+    useEffect(()=>{
+        axios
+        .get(`${process.env.REACT_APP_API}/blog/${props.match.params.slug}`)
+        .then(response=>{
+            setBlog(response.data)
+        })
+        .catch(err=>alert(err))
+    },[])
+
     return(
-        <div>Single Component</div>
+        <div className="container p-5">
+            <NavbarComponent />
+            <h1>{blog.title}</h1>
+            <p>{blog.content}</p>
+            <p className='text-muted'>ผู้เขียน: {blog.author} , เผยแพร่ : {new Date(blog.createdAt).toLocaleDateString()}</p>
+        </div>
     )
 }
 
